7229287ce3c2261204b4afaee452f63824f38cd1
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-05-01 21:23:47 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.02 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = {};
+        int count = 0;
+        for(int i = 0 ; i < numlist.length;i++){
+            if(numlist[i]%n == 0)count++;
+        }
+        answer = new int[count];
+        int j = 0;
+        for(int i = 0 ; i < numlist.length;i++){
+            if(numlist[i]%n == 0){
+                answer[j++] = numlist[i];
+            }
+        }
+        return answer;
+    }
+}
