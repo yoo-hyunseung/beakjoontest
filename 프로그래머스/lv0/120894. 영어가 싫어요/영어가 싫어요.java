ea3f38bcbf359e374ac8f82b5523acbbbd5e2e4e
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-05-01 20:51:51 +0900

    [level 0] Title: 영어가 싫어요, Time: 7.26 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        String [] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0 ; i < arr.length; i++){
+            numbers = numbers.replaceAll(arr[i],(i+""));
+        }
+        answer = Long.parseLong(numbers);
+        return answer;
+    }
+}
