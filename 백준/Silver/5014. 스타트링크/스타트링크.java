31b7cb1c4eaa8598b0083162b42ec3274a844b72
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-05-07 19:26:06 +0900

    [Silver I] Title: 스타트링크, Time: 204 ms, Memory: 57324 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,39 +1,45 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main{
-    static int visit[];
+    static int F,G,S,U,D;
+    static int count[];
+
     public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int F = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-        int G = Integer.parseInt(st.nextToken());
-        int U = Integer.parseInt(st.nextToken());
-        int D = Integer.parseInt(st.nextToken());
-        visit = new int [F+1];
-        bfs(F,S,G,U,D);
-    }
-    static void bfs(int F, int S, int G, int U, int D){
-        Queue <Integer>q = new LinkedList<>();
-        q.offer(S);
-        visit[S] = 1;
-        while(!q.isEmpty()){
-            int c = q.poll();
-            if(c == G){
-                System.out.println(visit[c] -1);
-            }
-            if(c + U <= F && visit[c+U] == 0){
-                visit[c+U] = visit[c]+1;
-                q.offer(c+U);
-            }
-            if(c-D > 0 && visit[c-D] ==0){
-                visit[c-D] = visit[c]+1;
-                q.offer(c-D);
-            }
-        }
-        if(visit[G] ==0){
+        F = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        G = Integer.parseInt(st.nextToken());
+        U = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+        count = new int[F+1];
+        BFS(S);
+        if(count[G] == 0){
             System.out.println("use the stairs");
+        }
+        else{
+            System.out.println(count[G]-1);
+        }
+
+
+    }
+    public static void BFS(int n){
+        Queue <Integer> queue = new LinkedList<>();
+        queue.offer(n);
+        count[n] = 1;
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            if(now == G)return;
+            if(now + U <= F && count[now+U] ==0){
+                count[now+U] = count[now]+1;
+                queue.offer(now+U);
+            }
+            if(now - D > 0 && count[now-D] == 0){
+                count[now-D] = count[now]+1;
+                queue.offer(now-D);
+            }
         }
     }
 }
