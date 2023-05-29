e561e227d850b7f2bc4d9f7febf82c636348a8a8
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-05-29 17:47:18 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args)throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("1");
+                System.out.println("0");
+    }
+   
+}
