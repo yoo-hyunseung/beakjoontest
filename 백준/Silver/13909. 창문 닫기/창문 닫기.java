03ce7b483339ca31aa4013fec1ed028b083e0e93
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-06-03 11:00:43 +0900

    [Silver V] Title: 창문 닫기, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = (int)Math.sqrt(n);
+        System.out.println(result);
+    }
+}
