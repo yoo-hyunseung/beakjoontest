90804baf42e9e2d75d03b568e3af8e048ba47c5e
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-06-04 12:49:59 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,37 +1,29 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main{
-    static int arr[];
     public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String str = br.readLine();
-        int N = str.length();
-        arr = new int[N];
-        for(int i = 0 ;i < N ; i++){
-            arr[i] = str.charAt(i)-'0';
+        String N = br.readLine();
+        int []num = new int[N.length()];
+        for(int i =0 ; i < N.length(); i++){
+            num[i] = Integer.parseInt(N.substring(i,i+1));
         }
-        int index = 0;
-        while(index < N){
-            int max = Integer.MIN_VALUE;
-            int fin = 0;
-            for(int i = index ; i<N ;i++){
-                if(max < arr[i]){
-                    max = arr[i];
-                    fin = i;
+        for(int i=0 ; i < N.length();i++){
+            int max = i;
+            for(int j= i+1; j < N.length();j++){
+                if(num[j]>num[max]){
+                    max = j;
                 }
             }
-
-            swap(index, fin);
-            sb.append(arr[index]);
-            index++;
+            if(num[max]>num[i]){
+                int temp = num[max];
+                num[max] = num[i];
+                num[i] = temp;
+            }
         }
-        System.out.println(sb);
-    }
-    public static void swap(int i , int j){
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+        for(int i =0 ; i < N.length();i++){
+            System.out.print(num[i]);
+        }
     }
 }
