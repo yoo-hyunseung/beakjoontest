6e805276251cfab9c7b4c31352b4d1da18798c5c
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-06-05 13:49:40 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1048 ms, Memory: 88324 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static List<Integer>list[];
+    static boolean[]visited;
+    static int[] count;
+    static int cnt;
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        list = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        count = new int[n+1];
+        for(int i =0 ; i < n+1; i++){
+            list[i] = new ArrayList<>();
+        }
+        for(int i =0 ; i < m ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+        }
+        for(int i =0 ; i < n+1 ; i++){
+            Collections.sort(list[i]);
+        }
+        bfs(r);
+        for(int i=1 ; i < count.length;i++){
+            sb.append(count[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void bfs(int r){
+        visited[r] = true;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(r);
+        while(!q.isEmpty()){
+            int num = q.poll();
+            cnt++;
+            count[num] = cnt;
+            for(int i =0 ; i < list[num].size(); i++){
+                int num2 = list[num].get(i);
+                if(!visited[num2]){
+                    visited[num2]= true;
+                    q.add(num2);
+                }
+            }
+
+        }
+    }
+}
