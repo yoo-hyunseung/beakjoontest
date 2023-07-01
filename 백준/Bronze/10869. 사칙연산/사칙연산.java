8938da365e1754c0289bec105435c22e72f87f01
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-07-01 20:00:18 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        sb.append(n+m).append('\n');
+        sb.append(n-m).append('\n');
+        sb.append(n*m).append('\n');
+        sb.append(n/m).append('\n');
+        sb.append(n%m).append('\n');
+        System.out.println(sb);
+    }
+    
+}
