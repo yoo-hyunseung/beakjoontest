dc34c3723831931b9dcd93793e346334689b83d2
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-07-12 23:54:06 +0900

    [Silver III] Title: 터렛, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main
+{
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb=new StringBuilder();
+        while(T-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int r1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            
+            sb.append(point(x1,y1,r1,x2,y2,r2)).append('\n');
+            
+                          
+    }
+        System.out.println(sb);
+}
+    public static int point(int x1,int y1,int r1,int x2,int y2,int r2){
+        int d = (int)(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        if(x1==x2 && y1==y2 && r1==r2)return -1;
+        else if(d > Math.pow(r1+r2,2))return 0;
+        else if(d < Math.pow(r2-r1,2))return 0;
+        else if(d == Math.pow(r2-r1,2))return 1;
+        else if(d == Math.pow(r1+r2,2))return 1;
+        else return 2;
+    }
+}
