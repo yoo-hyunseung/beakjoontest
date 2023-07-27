856549c9df1e8f82b3c3e13694064387fb507cf7
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-07-27 23:01:32 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int max = -1;
+        double sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i =0 ; i< N; i++){
+            int score = Integer.parseInt(st.nextToken());
+            if(max < score){
+                max = score;
+            }
+            sum += score;
+        }
+        double result = sum/max *100.0/N;
+        System.out.println(result);
+    }
+}
