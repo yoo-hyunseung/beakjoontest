1cc577404f45d46d3cb9540fb6abc6218a4a63c7
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-09-22 17:33:46 +0900

    [Bronze II] Title: 분해합, Time: 148 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,23 +1,25 @@
-import java.util.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class Main{
     public static void main(String[]args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());  
-        System.out.println(search(n));
-    }
-    public static int search(int n){
-        for(int i = 1; i < n ; i++){
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int result=0;
+        for(int i=1; i< M;i++){
             int sum = 0;
-            int k = i;
-            while(k!=0){
-                sum += k%10;
-                k/=10;
+            int n = i;
+            while(n!=0){
+                sum+= n%10;
+                n/=10;
             }
-            if(sum+i == n)
-                return i;
+            if(sum+i == M){
+                result =i;
+                break;
+            }
+            else result = 0;
         }
-        return 0;
+        System.out.println(result);
     }
 }
