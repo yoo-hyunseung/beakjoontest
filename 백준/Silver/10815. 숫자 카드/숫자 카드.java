d5157d021994b8a9a48f9ffffc4796819fb8d401
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-09-24 18:52:56 +0900

    [Silver V] Title: 숫자 카드, Time: 1288 ms, Memory: 113104 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,25 +1,48 @@
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
 public class Main{
-    public static void main(String[]args)throws IOException{
+    public static void main (String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        HashSet <Integer> set = new HashSet<>(n);
         StringTokenizer st;
-        st = new StringTokenizer(br.readLine()," ");
-        for(int i=0 ; i < n ; i++){
-            set.add(Integer.parseInt(st.nextToken()));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int [n];
+        st = new StringTokenizer(br.readLine(), " ");
+        
+        for(int i=0 ;i <n ;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine()," ");
-        for(int i=0 ; i< m ; i++){
-            if(set.contains(Integer.parseInt(st.nextToken())))
-                sb.append(1).append(" ");
-            else
-                sb.append(0).append(" ");
+        Arrays.sort(arr);
+        
+        int m = Integer.parseInt(br.readLine());        
+        int arr2 [] = new int[m];
+        st= new StringTokenizer(br.readLine()," ");
+        for(int i=0 ; i < m ;i++){
+            int target= Integer.parseInt(st.nextToken());
+            sb.append(BinSearch(arr,target)).append(" ");
         }
         System.out.println(sb);
+        
+    }
+    public static int BinSearch(int [] arr, int target){
+        int first = 0;
+        int last = arr.length-1;
+        int mid =0;
+        while(first <= last){
+            mid = (first + last)/2;
+            if(arr[mid] == target)return 1;
+            if(arr[mid] < target){
+                first = mid +1;
+            }
+            else{
+                last = mid-1;
+            }
+            
+        }
+        return 0;
     }
 }
