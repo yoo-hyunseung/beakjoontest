3a9b3871b7db8c1eff3521d147ea73ec36708393
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-10-01 15:07:05 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 344 ms, Memory: 20456 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int count = 0;
+    public static int recursion(String s, int l, int r){
+        count++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i =0 ; i < T ; i++){
+            String str = br.readLine();
+            System.out.println(isPalindrome(str)+" "+count);
+            count =0;
+        }
+    }
+}
