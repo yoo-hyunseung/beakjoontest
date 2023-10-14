972cf031d5b823ff9911cdc485081f3737227d14
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-10-14 14:47:05 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 756 ms, Memory: 48888 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -4,24 +4,24 @@ import java.util.*;
 public class Main{
     public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-
-        TreeMap <String, String> hm = new TreeMap<>();
+        HashMap <String, String> hashmap = new HashMap<>();
         for(int i =0 ; i < n ; i++){
             st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
             String state = st.nextToken();
-            if(hm.containsKey(name))
-                hm.remove(name);
-            else {
-                hm.put(name, state);
+            hashmap.put(name,state);
+            if(hashmap.get(name).equals("leave")){
+                hashmap.remove(name);
             }
         }
-        List<String> list = new ArrayList<>(hm.keySet());
-        Collections.sort(list);
-        for(int i = list.size()-1 ; i>=0;i--){
-            System.out.println(list.get(i));
+        List<String> list = new ArrayList<>(hashmap.keySet());
+        Collections.sort(list, Collections.reverseOrder());
+        for(String s : list){
+            sb.append(s).append("\n");
         }
+        System.out.println(sb);
     }
 }
