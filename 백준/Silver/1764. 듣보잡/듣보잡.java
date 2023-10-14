714b7f7760d2e249f4148d236bd746e51283503b
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-10-14 16:24:39 +0900

    [Silver IV] Title: 듣보잡, Time: 304 ms, Memory: 29000 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,29 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main{
     public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        HashSet<String> set = new HashSet<>();
-        ArrayList<String>list = new ArrayList<>();
-        
-        for(int i = 0 ; i<n;i++){
+        HashSet <String> set = new HashSet<>();
+        for(int i =0 ; i<n ; i++){
             set.add(br.readLine());
-        }
-        for(int i = 0 ; i<m;i++){
-            String str = br.readLine();
-            if(set.contains(str)){
-                list.add(str);
+        } 
+        int count =0;
+        List<String> result = new ArrayList<>();
+        for(int i =0 ; i <m ; i++){
+            String name = br.readLine();
+            if(!set.add(name)){
+                count++;
+                result.add(name);
             }
         }
-        Collections.sort(list);
-        sb.append(list.size()).append('\n');
-        for(String s : list){
-            sb.append(s).append('\n');
+        sb.append(count).append("\n");
+        Collections.sort(result);
+        for(String s : result){
+            sb.append(s).append("\n");
         }
         System.out.println(sb);
     }
