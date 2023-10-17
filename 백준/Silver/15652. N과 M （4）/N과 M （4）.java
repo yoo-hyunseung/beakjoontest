5b3041802f2a5d74487eacbb3d0d4fb949232a6c
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-10-17 19:36:18 +0900

    [Silver III] Title: N과 M (4), Time: 156 ms, Memory: 16660 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static int [] arr;
+    public static int n;
+    public static int m;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());   
+        arr = new int[m];
+        dfs(0,0);
+        System.out.println(sb);
+    }
+    public static void dfs(int at, int depth){
+        if(m == depth){
+            for(int val : arr)sb.append(val).append(" ");
+            sb.append('\n');
+            return;
+        }
+        for(int i=at ; i< n ; i++){
+            arr[depth]=i+1;
+            dfs(i,depth+1);
+        }
+    }
+}
