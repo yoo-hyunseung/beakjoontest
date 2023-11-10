f601defd4c37d7894022ca95d30b0f28a9a1934a
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-11-10 18:51:34 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int n = (int)s.charAt(0);
+        System.out.println(n);
+    }
+}
