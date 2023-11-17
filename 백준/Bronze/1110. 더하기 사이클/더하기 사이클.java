b21e7e2137349c004998459b6842377ebcedbabb
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-11-17 16:05:44 +0900

    [Bronze I] Title: 더하기 사이클, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum;
+        int num = N;
+        int count = 0;
+        while(true){
+            
+            sum = N/10 + N%10;
+            N = sum%10 + (N%10)*10;
+            count++;
+            if(num == N)break;
+        }
+        System.out.println(count);
+        
+    }
+}
