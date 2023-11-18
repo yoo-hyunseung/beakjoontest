650f324273d125240f526314288c3bf9b11580b7
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2023-11-18 16:11:14 +0900

    [Silver II] Title: 연속합, Time: 284 ms, Memory: 24492 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] arr = new int [N];
+        int [] dp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i =0 ; i< N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0] = arr[0];
+        int max = dp[0];
+        for(int i =1; i <N;i++){
+            dp[i] = Math.max((dp[i-1]+arr[i]),arr[i]);
+            max = Math.max(dp[i],max);
+        }
+        System.out.println(max);
+    }
+}
