8fa666d5f6a9cec710e68061d338c94620275aec
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2024-01-16 18:57:34 +0900

    [level Lv.2] Title: 2 x n 타일링, Time: 5.63 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/Lv.2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -5,17 +5,20 @@ class Solution {
         dp = new int[n+1];
         dp[1] = 1;
         dp[2] = 2;
-        for(int i =3 ; i < dp.length;i++){
-            dp[i] = (dp[i-1]+dp[i-2])%1000000007;
-        }
-        // answer = tile(n);
-        answer = dp[n];
+        // for(int i =3 ; i < dp.length;i++){
+        //     dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        // }
+        answer = tile(n);
+        // answer = dp[n];
         return answer;
     }
     public static int tile(int n){
         if(n<=2){
             return dp[n];
         }
-        return dp[n] = tile(n-1)%1000000007 +tile(n-2)%1000000007;
+        if(dp[n]!=0){
+            return dp[n]%1000000007;
+        }
+        return dp[n] = (tile(n-1)+tile(n-2))%1000000007;
     }
 }
