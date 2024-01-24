f3dea8941065b026aa921d5c4b3efabddc6f5e4a
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2024-01-24 20:46:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.57 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,25 +2,21 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String,Integer> map = new HashMap<>();
-        for(int i =0 ; i< participant.length;i++){
-            if(map.containsKey(participant[i])){
-                map.put(participant[i],map.get(participant[i])+1);
-            } else{
-                map.put(participant[i],1);
+        HashMap<String,Integer> hm = new HashMap<>();
+        for(int i=0 ; i < participant.length;i++){
+            hm.put(participant[i],hm.getOrDefault(participant[i],0)+1);
+        }
+        for(int i=0 ; i< completion.length;i++){
+            hm.put(completion[i],hm.get(completion[i])-1);
+        }
+        for(int i=0 ; i < participant.length;i++){
+            int num = hm.get(participant[i]);
+            if(num == 1){
+                answer = participant[i];
+                break;
             }
         }
-        for(int i =0 ; i < completion.length;i++){
-            if(map.containsKey(completion[i])){
-                map.put(completion[i],map.get(completion[i])-1);
-            }
-        }
-        for(String s : map.keySet()){
-            if(map.get(s)!=0){
-                answer+=s;
-            }
-        }
-        
+
         return answer;
     }
 }
