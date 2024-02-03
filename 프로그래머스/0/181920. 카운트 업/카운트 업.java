db5ade69481ac2818dc0ea7e07f5ff33a9739955
yoo-hyunseung <111487108+yoo-hyunseung@users.noreply.github.com>
2024-02-03 20:20:04 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = {};
+        answer = new int[end_num-start_num+1];
+        for(int i = start_num;i <= end_num;i++){
+            answer[i-start_num] = i;
+        }
+        return answer;
+    }
+}
